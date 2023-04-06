0cbb20a3903c1bcdd2e7d037d888c0043d78b7fa
DegenerateCoder <95440121+DegenerateCoder@users.noreply.github.com>
2023-04-06 06:31:03 +0200

    Move to gql twitch API (improved data reliability)

--- main.py
+++ main.py
@@ -1,60 +1,109 @@
 import requests
 import time
 from datetime import datetime
+import json
 
 
-def is_twitch_channel_live(channel_response):
-    live_identifiers = ["<script type=\"application/ld+json\">",
-                        "https://static-cdn.jtvnw.net/previews-ttv/live_user_"]
-    matched_id_count = 0
-    for identifier in live_identifiers:
-        if identifier in channel_response.text:
-            matched_id_count += 1
+def set_gql_twitch_clientid():
+    global client_id
+    url = "https://www.twitch.tv/"
+    response = requests.get(url)
+    
+    index = response.text.find("clientId=\"") 
+    index += len("clientId=\"")
+    end_index = response.text.find("\"",index)
+    
+    client_id = response.text[index:end_index]
+    
 
-    return matched_id_count == len(live_identifiers)
-
-
-def extract_livestream_title(channel_response):
-    description_tag = "<meta property=\"og:description\" content=\""
-
-    des_start_index = channel_response.text.find(description_tag)
-    des_end_index = 0
-    if (des_start_index != -1):
-        des_start_index += len(description_tag)
-        des_end_index = channel_response.text.find("\"", des_start_index)
-    else:  # different order of attributes
-        description_tag = "\" property=\"og:description\"/>"
-        des_end_index = channel_response.text.find(description_tag)
-        des_start_index = channel_response.text.rfind("\"", 0, des_end_index)
-        des_start_index += 1
-
-    livestream_title = channel_response.text[des_start_index:des_end_index]
-    return livestream_title
-
-
-def update_twitch_channel_status(channel_name):
-    channel_url = f"https://www.twitch.tv/{channel_name}"
+def is_twitch_channel_live(channel_name):
+    channel_url = f"https://gql.twitch.tv/gql"
     headers = {"Cache-Control": "no-cache, no-store, must-revalidate",
                "Pragma": "no-cache",
                "Expires": "0",
                "Surrogate-Control": "no-store",
-               "Vary": "*"}
-    response = requests.get(channel_url, headers=headers)
-    # sometimes twitch returns generic html response instead of channel one
-    while not (channel_name in response.text):
-        response = requests.get(channel_url, headers=headers)
+               "Vary": "*",
+               "Client-Id": f"{client_id}"}
 
+    json_request = """
+            [
+                {
+                    "operationName": "UseLive",
+                    "variables": {
+                        "channelLogin": ""
+                    },
+                    "extensions": {
+                        "persistedQuery": {
+                            "version": 1,
+                            "sha256Hash": "639d5f11bfb8bf3053b424d9ef650d04c4ebb7d94711d644afb08fe9a0fad5d9"
+                        }
+                    }
+                }
+            ]
+    """
+    json_request = json.loads(json_request)
+    json_request[0]['variables']['channelLogin'] = channel_name
+
+    response = requests.post(channel_url, headers=headers, json=json_request)
     if response.status_code != 200:
         print(
             f"Request error: {response.status_code}:{response.reason} for channel: {channel_name}")
         raise RuntimeError(
             f"Request failed: {response.status_code} reason: {response.reason}")
 
-    is_live = is_twitch_channel_live(response)
+    return response.json()[0]['data']['user']['stream'] != None
+
+
+def get_livestream_title(channel_name):
+    channel_url = f"https://gql.twitch.tv/gql"
+    headers = {"Cache-Control": "no-cache, no-store, must-revalidate",
+               "Pragma": "no-cache",
+               "Expires": "0",
+               "Surrogate-Control": "no-store",
+               "Vary": "*",
+               "Client-Id": f"{client_id}"}
+
+    json_request = """
+            [
+                {
+                    "operationName": "FilterableVideoTower_Videos",
+                    "variables": {
+                        "limit": 1,
+                        "channelOwnerLogin": "",
+                        "broadcastType": "ARCHIVE",
+                        "videoSort": "TIME"
+                    },
+                    "extensions": {
+                        "persistedQuery": {
+                            "version": 1,
+                            "sha256Hash": "a937f1d22e269e39a03b509f65a7490f9fc247d7f83d6ac1421523e3b68042cb"
+                        }
+                    }
+                }
+            ]
+    """
+    json_request = json.loads(json_request)
+    json_request[0]['variables']['channelOwnerLogin'] = channel_name
+
+    response = requests.post(channel_url, headers=headers, json=json_request)
+    if response.status_code != 200:
+        print(
+            f"Request error: {response.status_code}:{response.reason} for channel: {channel_name}")
+        raise RuntimeError(
+            f"Request failed: {response.status_code} reason: {response.reason}")
+
+    vid_title = response.json(
+    )[0]['data']['user']['videos']['edges'][0]['node']['title']
+    return vid_title
+
+
+def update_twitch_channel_status(channel_name):
+    is_live = is_twitch_channel_live(channel_name)
     if (twitch_channels[channel_name] != is_live):
         twitch_channels[channel_name] = is_live
         if (is_live):
-            title = extract_livestream_title(response)
+            title = get_livestream_title(channel_name)
+            channel_url = f"https://www.twitch.tv/{channel_name}"
             message = f"{channel_name} is Live! {title} \n{channel_url}"
             notify(message)
         else:
@@ -87,6 +136,8 @@ if __name__ == "__main__":
         "channel1": False,
         "channel2": False
     }
+
+    set_gql_twitch_clientid()
 
     while (True):
         monitor_twitch_channels()
