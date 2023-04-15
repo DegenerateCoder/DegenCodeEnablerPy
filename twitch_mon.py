6d389c2d1d29f56cf20fcb7136bac21d90cae1a2
DegenerateCoder <95440121+DegenerateCoder@users.noreply.github.com>
2023-04-16 01:24:13 +0200

    Fix wrong/old twitch live stream title (usage of ComscoreStreamingQuery instead of vod data)

--- twitch_mon.py
+++ twitch_mon.py
@@ -4,11 +4,11 @@ import json
 from datetime import datetime
 from dataclasses import dataclass
 
+
 @dataclass
 class TwitchChannel:
     name: str
     live: bool = False
-    livestream_vod_url: str = ""
 
 
 twitch_channels = [
@@ -29,8 +29,8 @@ def set_gql_twitch_clientid():
     client_id = response.text[index:end_index]
 
 
-def is_twitch_channel_live(channel_name: str):
-    channel_url = f"https://gql.twitch.tv/gql"
+def is_twitch_channel_live(channel_name: str) -> bool:
+    twitch_gpl_url = f"https://gql.twitch.tv/gql"
     headers = {"Cache-Control": "no-cache, no-store, must-revalidate",
                "Pragma": "no-cache",
                "Expires": "0",
@@ -39,36 +39,34 @@ def is_twitch_channel_live(channel_name: str):
                "Client-Id": f"{client_id}"}
 
     json_request = """
-            [
-                {
-                    "operationName": "UseLive",
-                    "variables": {
-                        "channelLogin": ""
-                    },
-                    "extensions": {
-                        "persistedQuery": {
-                            "version": 1,
-                            "sha256Hash": "639d5f11bfb8bf3053b424d9ef650d04c4ebb7d94711d644afb08fe9a0fad5d9"
-                        }
-                    }
+        {
+            "operationName": "UseLive",
+            "variables": {
+                "channelLogin": ""
+            },
+            "extensions": {
+                "persistedQuery": {
+                    "version": 1,
+                    "sha256Hash": "639d5f11bfb8bf3053b424d9ef650d04c4ebb7d94711d644afb08fe9a0fad5d9"
                 }
-            ]
+            }
+        }
     """
     json_request = json.loads(json_request)
-    json_request[0]['variables']['channelLogin'] = channel_name
+    json_request['variables']['channelLogin'] = channel_name
 
-    response = requests.post(channel_url, headers=headers, json=json_request)
+    response = requests.post(twitch_gpl_url, headers=headers, json=json_request)
     if response.status_code != 200:
         print(
             f"Request error: {response.status_code}:{response.reason} for channel: {channel_name}")
         raise RuntimeError(
             f"Request failed: {response.status_code} reason: {response.reason}")
 
-    return response.json()[0]['data']['user']['stream'] != None
+    return response.json()['data']['user']['stream'] != None
 
 
-def get_livestream_title_and_vodid(channel_name: str):
-    channel_url = f"https://gql.twitch.tv/gql"
+def get_livestream_title(channel_name: str) -> str:
+    twitch_gpl_url = f"https://gql.twitch.tv/gql"
     headers = {"Cache-Control": "no-cache, no-store, must-revalidate",
                "Pragma": "no-cache",
                "Expires": "0",
@@ -77,54 +75,90 @@ def get_livestream_title_and_vodid(channel_name: str):
                "Client-Id": f"{client_id}"}
 
     json_request = """
-            [
-                {
-                    "operationName": "FilterableVideoTower_Videos",
-                    "variables": {
-                        "limit": 1,
-                        "channelOwnerLogin": "",
-                        "broadcastType": "ARCHIVE",
-                        "videoSort": "TIME"
-                    },
-                    "extensions": {
-                        "persistedQuery": {
-                            "version": 1,
-                            "sha256Hash": "a937f1d22e269e39a03b509f65a7490f9fc247d7f83d6ac1421523e3b68042cb"
-                        }
-                    }
+        {
+            "operationName": "ComscoreStreamingQuery",
+            "variables": {
+                "channel": "",
+                "clipSlug": "",
+                "isClip": false,
+                "isLive": true,
+                "isVodOrCollection": false,
+                "vodID": ""
+            },
+            "extensions": {
+                "persistedQuery": {
+                    "version": 1,
+                    "sha256Hash": "e1edae8122517d013405f237ffcc124515dc6ded82480a88daef69c83b53ac01"
                 }
-            ]
+            }
+        }
     """
     json_request = json.loads(json_request)
-    json_request[0]['variables']['channelOwnerLogin'] = channel_name
+    json_request['variables']['channel'] = channel_name
 
-    response = requests.post(channel_url, headers=headers, json=json_request)
+    response = requests.post(twitch_gpl_url, headers=headers, json=json_request)
     if response.status_code != 200:
         print(
             f"Request error: {response.status_code}:{response.reason} for channel: {channel_name}")
         raise RuntimeError(
             f"Request failed: {response.status_code} reason: {response.reason}")
 
-    vid_json = response.json()[0]['data']['user']['videos']['edges'][0]['node']
-    vid_title = vid_json['title']
+    vid_title = response.json()['data']['user']['broadcastSettings']['title']
+    return vid_title
+
+
+def get_livestream_vodid(channel_name: str) -> str:
+    twitch_gpl_url = f"https://gql.twitch.tv/gql"
+    headers = {"Cache-Control": "no-cache, no-store, must-revalidate",
+               "Pragma": "no-cache",
+               "Expires": "0",
+               "Surrogate-Control": "no-store",
+               "Vary": "*",
+               "Client-Id": f"{client_id}"}
+
+    json_request = """
+        {
+            "operationName": "FilterableVideoTower_Videos",
+            "variables": {
+                "limit": 1,
+                "channelOwnerLogin": "",
+                "broadcastType": "ARCHIVE",
+                "videoSort": "TIMEget_livestream_title
+            },
+            "extensions": {
+                "persistedQuery": {
+                    "version": 1,
+                    "sha256Hash": "a937f1d22e269e39a03b509f65a7490f9fc247d7f83d6ac1421523e3b68042cb"
+                }
+            }
+        }
+    """
+    json_request = json.loads(json_request)
+    json_request['variables']['channelOwnerLogin'] = channel_name
+
+    response = requests.post(twitch_gpl_url, headers=headers, json=json_request)
+    if response.status_code != 200:
+        print(
+            f"Request error: {response.status_code}:{response.reason} for channel: {channel_name}")
+        raise RuntimeError(
+            f"Request failed: {response.status_code} reason: {response.reason}")
+
+    vid_json = response.json()['data']['user']['videos']['edges'][0]['node']
     vod_id = vid_json['id']
-    return (vid_title, vod_id)
+    return vod_id
 
 
 def update_twitch_channel_status(channel: TwitchChannel):
     is_live = is_twitch_channel_live(channel.name)
     if (channel.live != is_live):
         if (is_live):
-            title_vodid = get_livestream_title_and_vodid(channel.name)
-            title = title_vodid[0]
-            vod_id = title_vodid[1]
-            channel.livestream_vod_url = f"https://www.twitch.tv/videos/{vod_id}"
+            title = get_livestream_title(channel.name)
             channel_url = f"https://www.twitch.tv/{channel.name}"
             message = f"{channel.name} is Live! {title} \n{channel_url}"
             notify(message)
         else:
-            message = f"{channel.name} has gone offline \n{channel.livestream_vod_url}"
-            channel.livestream_vod_url = ""
+            vod_id = get_livestream_vodid(channel.name)
+            message = f"{channel.name} has gone offline \nhttps://www.twitch.tv/videos/{vod_id}"
             notify(message)
         channel.live = is_live
 
